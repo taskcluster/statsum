f633bd4fb971a275337f91e55b4dd2807d24d954
Jonas Finnemann Jensen <jopsen@gmail.com>
2017-07-06 13:47:36 -0700

    Use signalfx rest API

--- server/signalfx.go
+++ server/signalfx.go
@@ -1,26 +1,32 @@
 package server
 
 import (
+	"fmt"
+	"net/http"
 	"time"
 
-	"golang.org/x/net/context"
-
-	"github.com/signalfx/golib/datapoint"
-	"github.com/signalfx/golib/sfxclient"
+	"github.com/pkg/errors"
+	got "github.com/taskcluster/go-got"
 )
+
+const signalfxEndPoint = "https://ingest.signalfx.com/v2/datapoint"
+
+type datapoint struct {
+	Metric    string  `json:"metric"`
+	Value     float64 `json:"value"`
+	Timestamp int64   `json:"timestamp,omitempty"`
+}
 
 type signalfxDrain struct {
 	token  string
-	client *sfxclient.HTTPDatapointSink
-	points []*datapoint.Datapoint
+	g      *got.Got
+	points []datapoint
 }
 
 func newSignalfxDrain(token string) *signalfxDrain {
-	client := sfxclient.NewHTTPDatapointSink()
-	client.AuthToken = token
 	return &signalfxDrain{
-		token:  token,
-		client: client,
+		token: token,
+		g:     got.New(),
 	}
 }
 
@@ -29,13 +35,28 @@ func (s *signalfxDrain) Name() string {
 }
 
 func (s *signalfxDrain) Send(name string, value float64, time time.Time) {
-	dataPoint := datapoint.New(name, nil, datapoint.NewFloatValue(value), datapoint.Gauge, time)
-	s.points = append(s.points, dataPoint)
+	s.points = append(s.points, datapoint{
+		Metric:    name,
+		Value:     value,
+		Timestamp: time.Unix() * 1000,
+	})
 }
 
 func (s *signalfxDrain) Flush() error {
-	ctx := context.Background()
-	points := s.points
-	s.points = nil
-	return s.client.AddDatapoints(ctx, points)
+	req := s.g.Post(signalfxEndPoint, nil)
+	err := req.JSON(map[string][]datapoint{
+		"gauge": s.points,
+	})
+	if err != nil {
+		panic(errors.Wrap(err, "failed to marshal payload to JSON"))
+	}
+	req.Header.Set("X-SF-Token", s.token)
+	res, err := req.Send()
+	if err != nil {
+		return err
+	}
+	if res.StatusCode != http.StatusOK {
+		return fmt.Errorf("Wrong status code from signalfx: %d", res.StatusCode)
+	}
+	return nil
 }
